99a66fcfa7decffadcf1d5490bb8295cc784ec39
shoku10 <shoku8544@gmail.com>
2023-09-13 23:12:21 +0900

    Initial Commit

---
+++ BurnDownChartsApp/BurnDownChartsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnDownChartsAppApp.swift
+//  BurnDownChartsApp
+//
+//  Created by USER on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct BurnDownChartsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
